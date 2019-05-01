bda93da3a02b57d45d7f55b7a5e16f1a78883796
Valentin Trusevich <v.trusevich@itransition.com>
2019-05-01 16:21:33 +0300

    Added swagger for all microservices

--- src/main/kotlin/by/trusevich/house/core/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/by/trusevich/house/core/exception/handler/GlobalExceptionHandler.kt
@@ -12,34 +12,33 @@ import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.http.HttpStatus.UNAUTHORIZED
-import org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY
 import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.bind.annotation.RequestMethod.DELETE
+import org.springframework.web.bind.annotation.RequestMethod.GET
+import org.springframework.web.bind.annotation.RequestMethod.PATCH
+import org.springframework.web.bind.annotation.RequestMethod.POST
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
-@RestControllerAdvice
+@ControllerAdvice
+@CrossOrigin(origins = ["*"], allowedHeaders = ["*"], methods = [GET, POST, DELETE, PATCH])
 class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(EmptyResultDataAccessException::class, EntityNotFoundException::class)
-    @ResponseStatus(NOT_FOUND)
     fun handleNotFound(ex: Exception) = toResponse(ex, NOT_FOUND)
 
     @ExceptionHandler
-    @ResponseStatus(NO_CONTENT)
     fun handle(ex: EntityNoContentException) = toResponse(ex, NO_CONTENT)
 
     @ExceptionHandler
-    @ResponseStatus(UNAUTHORIZED)
     fun handle(ex: UnauthorizedException) = toResponse(ex, UNAUTHORIZED)
 
     @ExceptionHandler
-    @ResponseStatus(INTERNAL_SERVER_ERROR)
     fun globalExceptionHandler(ex: Exception) = toResponse(ex, INTERNAL_SERVER_ERROR)
 
-    @ResponseStatus(UNPROCESSABLE_ENTITY)
     override fun handleMethodArgumentNotValid(
         ex: MethodArgumentNotValidException, headers: HttpHeaders, status: HttpStatus, request: WebRequest
     ) = toValidationResponse(ex)
--- src/main/kotlin/by/trusevich/house/core/util/ExceptionHandlingUtils.kt
+++ src/main/kotlin/by/trusevich/house/core/util/ExceptionHandlingUtils.kt
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY
 import org.springframework.http.MediaType.APPLICATION_JSON_UTF8
 import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.status
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
 import java.util.Date
@@ -17,18 +18,19 @@ private val log by lazyLogger("ExceptionHandlingUtils")
 @Suppress("unused")
 const val SC_UNPROCESSABLE_ENTITY = 422
 
-fun toResponse(ex: Exception, status: HttpStatus) = toResponse(ex, ex.message, status)
+fun toResponse(ex: Exception, status: HttpStatus): ResponseEntity<ErrorDetails> = status(status).body(toResponse(ex, ex.message, status))
 
 fun toValidationResponse(ex: MethodArgumentNotValidException): ResponseEntity<Any> =
-    ResponseEntity.status(UNPROCESSABLE_ENTITY).contentType(APPLICATION_JSON_UTF8).body(toResponse(ex,
-        VALIDATION_REASON,
-        UNPROCESSABLE_ENTITY,
-        ex.bindingResult.allErrors.mapWithTransform { from, to ->
-            to.resource = from.objectName
-            to.field = (from as FieldError).field
-            to.code = from.code
-            to.message = from.defaultMessage
-        }) as Any
+    status(UNPROCESSABLE_ENTITY).contentType(APPLICATION_JSON_UTF8).body(
+        toResponse(ex,
+            VALIDATION_REASON,
+            UNPROCESSABLE_ENTITY,
+            ex.bindingResult.allErrors.mapWithTransform { from, to ->
+                to.resource = from.objectName
+                to.field = (from as FieldError).field
+                to.code = from.code
+                to.message = from.defaultMessage
+            }) as Any
     )
 
 @Suppress("UNCHECKED_CAST")
